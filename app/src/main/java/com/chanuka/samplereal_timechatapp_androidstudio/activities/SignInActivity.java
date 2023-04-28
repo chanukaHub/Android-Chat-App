60ad1c81d1f44a9d736416b1994f2d27141db481
chanukaHub <chanukadil1999@gmail.com>
2023-04-28 18:41:09 +0530

    finished signUp signIn Ui

--- app/src/main/java/com/chanuka/samplereal_timechatapp_androidstudio/activities/SignInActivity.java
+++ app/src/main/java/com/chanuka/samplereal_timechatapp_androidstudio/activities/SignInActivity.java
@@ -2,15 +2,26 @@ package com.chanuka.samplereal_timechatapp_androidstudio.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.chanuka.samplereal_timechatapp_androidstudio.R;
+import com.chanuka.samplereal_timechatapp_androidstudio.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
     }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+            startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        }
 }
--- app/src/main/java/com/chanuka/samplereal_timechatapp_androidstudio/activities/SignUpActivity.java
+++ app/src/main/java/com/chanuka/samplereal_timechatapp_androidstudio/activities/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.chanuka.samplereal_timechatapp_androidstudio.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.chanuka.samplereal_timechatapp_androidstudio.R;
+import com.chanuka.samplereal_timechatapp_androidstudio.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
     }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        }
 }
