a331c89b2dbaacfd8cd5f867920a64aff7eb8a38
chanukaHub <chanukadil1999@gmail.com>
2023-04-28 12:47:27 +0530

    Initial commit

---
+++ app/src/main/java/com/chanuka/samplereal_timechatapp_androidstudio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chanuka.samplereal_timechatapp_androidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
