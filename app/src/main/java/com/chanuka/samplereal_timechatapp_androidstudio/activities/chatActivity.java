7ae3a1e31a4f0c4ffc0cce22af9458a57390753e
chanukaHub <chanukadil1999@gmail.com>
2023-05-03 19:01:16 +0530

    added UI components

---
+++ app/src/main/java/com/chanuka/samplereal_timechatapp_androidstudio/activities/chatActivity.java
@@ -0,0 +1,16 @@
+package com.chanuka.samplereal_timechatapp_androidstudio.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chanuka.samplereal_timechatapp_androidstudio.R;
+
+public class chatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
